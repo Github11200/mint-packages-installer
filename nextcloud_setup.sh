9991b7656d9346fea1a056fc173f68955940e517
Github11200 <pateljinay@outlook.com>
2026-08-20 18:15:33 -0700

    updated nextcloud script

--- nextcloud_setup.sh
+++ nextcloud_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "==========================\nInstalling Docker\n==========================\n"
+echo -e "==========================\nInstalling Docker\n==========================\n"
 
 # Add Docker's official GPG key:
 sudo apt update
@@ -21,19 +21,15 @@ EOF
 
 sudo apt update
 
-echo "==========================\nInstalling docker packages\n==========================\n"
+echo -e "\n==========================\nInstalling docker packages\n==========================\n"
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
-echo "==========================\nRunning the Nextcloud Docker container\n==========================\n"
+echo -e "\n==========================\nRunning the Nextcloud Docker container\n==========================\n"
 
 # Get the ID of the container that we just started
-CONTAINER_ID=$(docker run -p -d 8080:80 nextcloud)
-echo "Using container ID: $CONTAINER_ID"
-docker exec --user www-data $CONTAINER_ID php /var/www/html/occ config:system:set trusted_domains 1 --value=10.42.0.1:8080
+CONTAINER_ID=$(docker run -d -p 8080:80 -e NEXTCLOUD_TRUSTED_DOMAINS="10.42.0.1" nextcloud)
+echo -e "\nUsing container ID: $CONTAINER_ID\n"
 
-# Print out the file to make sure it was written to correctly
-docker exec --user www-data $CONTAINER_ID cat /var/www/html/config/config.php
-
-echo "==========================\nOpening link\n==========================\n"
+echo -e "\n==========================\nOpening link\n==========================\n"
 xdg-open "http://localhost:8080"
