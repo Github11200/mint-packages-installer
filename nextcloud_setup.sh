b9bd4c33a6c538758ea232fadc1f70fdc9a5bdaa
Github11200 <pateljinay@outlook.com>
2026-08-20 17:57:51 -0700

    updated how I get the ID

--- nextcloud_setup.sh
+++ nextcloud_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Installing Docker..."
+echo "==========================\nInstalling Docker\n==========================\n"
 
 # Add Docker's official GPG key:
 sudo apt update
@@ -21,20 +21,19 @@ EOF
 
 sudo apt update
 
-echo "Installing docker packages..."
+echo "==========================\nInstalling docker packages\n==========================\n"
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
-echo "Running the Nextcloud Docker container..."
-docker run -p -d 8080:80 nextcloud
+echo "==========================\nRunning the Nextcloud Docker container\n==========================\n"
 
 # Get the ID of the container that we just started
-CONTAINER_ID=$(docker ps -q | head -n 1)
+CONTAINER_ID=$(docker run -p -d 8080:80 nextcloud)
 echo "Using container ID: $CONTAINER_ID"
 docker exec --user www-data $CONTAINER_ID php /var/www/html/occ config:system:set trusted_domains 1 --value=10.42.0.1:8080
 
 # Print out the file to make sure it was written to correctly
 docker exec --user www-data $CONTAINER_ID cat /var/www/html/config/config.php
 
-echo "Opening link..."
+echo "==========================\nOpening link\n==========================\n"
 xdg-open "http://localhost:8080"
