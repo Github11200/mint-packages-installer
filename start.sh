d3b4206f98fe6d0d9045532a407a1d5281eaca4f
Github11200 <pateljinay@outlook.com>
2026-08-20 22:01:00 -0700

    added sudos

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 echo -e "Starting container...\n"
-CONTAINER_ID=$(docker ps -aq | head -n 1)
-docker start $CONTAINER_ID
+CONTAINER_ID=$(sudo docker ps -aq | head -n 1)
+sudo docker start $CONTAINER_ID
 
 echo -e "Starting tailscale...\n"
+sudo tailscale up
 sudo tailscale funnel 8080
